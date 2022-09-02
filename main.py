95dbc5b55ce64e9de2bae8cfafce2db21106f563
Catalina-Elena Popa <74061561+catalinaeln7@users.noreply.github.com>
2022-09-02 16:04:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,127 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, shuffle, randint
+import pyperclip
+import json
+
+# ---------------------------- CONSTANTS ----------------------------------------#
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+# ----------------------------- SEARCH ENGINE -----------------------------------#
+def search_acc():
+    website = website_entry.get()
+
+    if len(website) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave the website field empty!")
+        return
+
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=f"{website}", message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message="There is no account for this website.")
+    finally:
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_pass():
+    letters_gen = [choice(letters) for _ in range(0, randint(8, 10))]
+    numbers_gen = [choice(numbers) for _ in range(0, randint(2, 4))]
+    symbols_gen = [choice(symbols) for _ in range(0, randint(2, 4))]
+
+    password_list = letters_gen + numbers_gen + symbols_gen
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    website = website_entry.get()
+    email = email_username_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+        return
+
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
+    else:
+        data.update(new_data)
+
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+    finally:
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# Window
+window = Tk()
+window.title("Password Manager")
+window.config(padx=30, pady=30, bg="white")
+
+# Canvas
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+locker_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=locker_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website:", bg="white")
+email_username_label = Label(text="Email/Username:", bg="white")
+password_label = Label(text="Password:", bg="white")
+
+website_label.grid(row=1, column=0)
+email_username_label.grid(row=2, column=0)
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=27)
+website_entry.focus()
+email_username_entry = Entry(width=45)
+password_entry = Entry(width=27)
+
+website_entry.grid(row=1, column=1)
+email_username_entry.grid(row=2, column=1, columnspan=2)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+search_button = Button(text="Search", bg="white", command=search_acc, width=14)
+generate_pass_button = Button(text="Generate Password", bg="white", command=generate_pass)
+add_button = Button(text="Add", width=39, bg="white", command=save)
+
+search_button.grid(row=1, column=2)
+generate_pass_button.grid(row=3, column=2)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
